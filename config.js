42cec634d18b3a5c63f820ef718017064a416d39
root <root@ip-172-31-81-18.ec2.internal>
2018-07-24 06:58:06 +0000

    remove credentials

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
   credentials: {
-    accessKeyId: "AKIAICW37NEQVZMQL2OQ",
-    secretAccessKey: "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"
   },
   options: { //Add the log file path without date and extension
     logfiles: ["/access-log/access-log", "/error-log/error-log", "/security-log/security-log"],
